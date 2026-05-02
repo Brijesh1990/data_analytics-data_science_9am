b15e118ef5506d7411b4226a0f88d12f13c8c542
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-05-02 09:30:58 +0530

    added

---
+++ assingment/python-assingmments/1-print sum postive_number.py
@@ -0,0 +1,6 @@
+number1=14
+number2=25
+number3=5
+number4=56
+result=number1+number2+number3+number4
+print("Sum of all positive numbers :",result)
---
+++ extra/2_karan.py
@@ -0,0 +1,8 @@
+a=80   
+b=80
+c=10
+e=50    
+print(a*b/c-e)
+print(a+b)
+print(a-b) 
+print(c/e)
