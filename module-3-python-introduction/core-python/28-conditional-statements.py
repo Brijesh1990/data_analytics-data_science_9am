8174dedf52f36be34c62a72251bc824d3e8edf0c
Brijesh1990 <bkpandey.pandey@gmail.com>
2026-05-16 10:03:25 +0530

    16-05-2026 all data uploaded

---
+++ module-3-python-introduction/core-python/28-conditional-statements.py
@@ -0,0 +1,12 @@
+# where we check true & false via statements there we used conditional statements 
+
+# conditional statements checked true & false 
+
+# types of conditional statements in python ....
+
+
+# 1. if statements 
+# 2. if - else statements 
+# 3. nested if statements  
+# 4. if elif statements 
+# 5. switch (not used in python)
---
+++ module-3-python-introduction/core-python/32-nested-if.py
@@ -0,0 +1,22 @@
+# condition within another condition there we used nested if conditions 
+#  if within another if i.e called nested if 
+
+# syntax 
+
+# if condition:
+#     if condition:
+#         statements
+        
+# else:
+#     statements
+
+a=10
+b=5
+if a>b:
+    if a!=0 and b!=0:
+        print("a is greater than b and both are positive number")
+else:
+    print("a is less than b")
+        
+
+
